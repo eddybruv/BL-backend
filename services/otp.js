0b084f8e3eb584b295ae7385aa7efd2bc122d32e
eddybruv <edwinajong@gmail.com>
2022-08-01 10:12:17 +0100

    otp generator

---
+++ services/otp.js
@@ -0,0 +1,9 @@
+const otpGenerator = require("otp-generator");
+const { OTP_LENGTH, OTP_CONFIG } = require("../constants/constants");
+
+const generateOTP = async () => {
+  const OTP = otpGenerator.generate(OTP_LENGTH, OTP_CONFIG);
+  return OTP;
+};
+
+module.exports = { generateOTP };
