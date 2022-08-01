14bd4cc951de5cea68ad88fa3910249eeae715ad
eddybruv <edwinajong@gmail.com>
2022-08-01 10:12:33 +0100

    mailing configuration

---
+++ services/mail.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+
+const MAIL_SETTINGS = {
+  service: "gmail",
+
+  auth: {
+    user: process.env.MAIL_EMAIL,
+    pass: process.env.MAIL_PASSWORD,
+  },
+};
+
+const nodemailer = require("nodemailer");
+const transporter = nodemailer.createTransport(MAIL_SETTINGS);
+
+const sendMail = async (params) => {
+  console.log("here");
+  try {
+    let info = await transporter.sendMail({
+      from: MAIL_SETTINGS.auth.pass,
+      to: params.to,
+      subject: "Hi there 😄",
+      html: `
+      <div
+        class="container"
+        style="max-width: 90%; margin: auto; padding-top: 20px"
+      >
+        <h2>Bridge Labs.</h2>
+        <h4>Welcome To The Team ✔</h4>
+        <p style="margin-bottom: 30px;">Your OTP to get started🎉</p>
+        <h1 style="font-size: 24px; letter-spacing: 2px; text-align:center;">${params.OTP}</h1>
+        <p style="margin-top:50px;">If you did not request for verification please ignore the mail.</p>
+      </div>
+    `,
+    });
+    return info;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
+
+module.exports = { sendMail };
