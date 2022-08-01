4c496c5fd012c3a0193c5853f6ef9d4f30af6d6b
eddybruv <edwinajong@gmail.com>
2022-08-01 10:12:49 +0100

    encryption and comparism

---
+++ services/bcrypt.js
@@ -0,0 +1,23 @@
+const bcrypt = require("bcrypt");
+const saltRounds = 12;
+
+const encrypt = async (password) => {
+  try {
+    if (password === undefined) {
+      console.log("not defined")
+      return false;
+    }
+    const salt = await bcrypt.genSalt(saltRounds);
+    const encryptedPwd = await bcrypt.hash(password, salt);
+    return encryptedPwd;
+  } catch (up) {
+    throw up;
+  }
+};
+
+const compare = async (password, encryptedPwd) => {
+  const value = await bcrypt.compare(password, encryptedPwd);
+  return value;
+};
+
+module.exports = { compare, encrypt };
