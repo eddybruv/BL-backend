8d4a25197c082bfa05e3d02c592d47f992a25efc
eddybruv <edwinajong@gmail.com>
2022-08-01 10:17:59 +0100

    database connection

---
+++ config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const connectDB = mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("🫙 [Database]: Up and Running");
+  })
+  .catch(() => console.log("🥲[Database]: Not connected"));
+
+module.exports = connectDB;
