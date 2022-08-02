d43d15d074688855e39ed39675da0c06dddc6559
eddybruv <edwinajong@gmail.com>
2022-08-02 21:21:50 +0100

    swagger fdefinition and api specifications

---
+++ docs/options.js
@@ -0,0 +1,42 @@
+const swaggerDefinition = {
+  info: {
+    version: "3.0.0",
+    title: "BL Backend API",
+    description: "Backend onboarding task for Bridge Labs",
+    license: {
+      name: "MIT",
+      url: "https://opensource.org/licenses/MIT",
+    },
+  },
+  swagger: "2.0",
+  contact: {
+    name: "Edwin Bimela Ajong",
+    email: "edwinajong@gmail.com",
+  },
+  host: "localhost:5000",
+  basePath: "/",
+  schemes: ["http"],
+  consumes: [
+    "application/json",
+    "multipart/form-data",
+    "application/x-www-form-urlencoded",
+  ],
+  securityDefinitions: {
+    bearerAuth: {
+      type: "apiKey",
+      name: "Authorization",
+      scheme: "bearer",
+      in: "header",
+    },
+  },
+  produces: ["application/json"],
+  paths: {},
+};
+
+const options = {
+  swaggerDefinition,
+  apis: ["./docs/*/*.js"],
+};
+
+
+module.exports = options;
