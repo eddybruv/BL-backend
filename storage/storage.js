f23ef5073f4c12a76049719dec8aa418bfa3dff5
eddybruv <edwinajong@gmail.com>
2022-08-02 21:17:01 +0100

    changed the folder name

--- storage/storage.js
+++ storage/storage.js
@@ -12,7 +12,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
-    folder: "BL Backend Users",
+    folder: "bl-users",
     allowedFormats: ["jpeg", "png", "jpg"],
   },
 });
