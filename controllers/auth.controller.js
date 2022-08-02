fea81771ea38d4effb62fbbb4b474103a388e47f
eddybruv <edwinajong@gmail.com>
2022-08-02 21:22:35 +0100

    added jwt and other controllers

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,6 +2,7 @@ const { encrypt, compare } = require("../services/bcrypt");
 const { generateOTP } = require("../services/otp.js");
 const { sendMail } = require("../services/mail");
 const { storage } = require("../storage/storage");
+const generateToken = require("../config/generateToken");
 const multer = require("multer");
 const upload = multer({ storage });
 const userModel = require("../models/User.model");
@@ -25,35 +26,59 @@ const createUser = async (name, email, password, username, profilePicture) => {
       to: email,
       OTP: otpGenerated,
     });
-    return [true, newUser];
+    return newUser;
   } catch (error) {
     return [false, "Unable to sign up because of mail error", error];
   }
 };
 
-const signUp =
-  (upload.single("image"),
-  async (req, res) => {
-    console.log(req.file);
-    const { email, password, username, profilePicture, name } = req.body;
-    const userExist = await userModel.findOne({ email });
-    if (userExist) {
-      return res.status(400).json({ message: "user already exists" });
-    }
-    const newUser = await createUser(
-      name,
-      email,
-      password,
-      username,
-      profilePicture
-    );
+const signUp = async (req, res) => {
+  const imagePath = await req.file?.path;
+  const { email, password, username, name } = req.body;
+  const userExist = await userModel.findOne({ email });
+  if (userExist) {
+    return res.status(400).json({ message: "user already exists" });
+  }
+  const newUser = await createUser(name, email, password, username, imagePath);
 
-    if (!newUser) {
-      res.status(400).json({ message: "user not created" });
-    } else {
-      res.json({ data: newUser });
-    }
-  });
+  if (!newUser) {
+    res.status(400).json({ message: "user not created" });
+  } else {
+    let token = generateToken(newUser._id);
+    res.json({ data: newUser, token });
+  }
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await userModel.findOne({ email });
+
+  if (!user) {
+    return res.json({ message: "user not found" }).status(400);
+  }
+
+  if (compare(password, user.password)) {
+    let token = generateToken(user._id);
+    return res.json({ data: user, token }).status(200);
+  } else {
+    return res.json({ message: "credentials don't match" }).status(400);
+  }
+};
+
+const logout = async (req, res) => {
+  const _id = req.userId;
+  const user = await userModel.findById({ _id });
+
+  if (!user) {
+    res.status(400).json({ message: "user not found" });
+  } else {
+    const updatedUser = await userModel.findByIdAndUpdate(
+      { _id },
+      { isActive: false, otp: null }
+    );
+    res.status(200).json({ message: "signed out" });
+  }
+};
 
 const validateSignUp = async (email, otp) => {
   const user = await userModel.findOne({ email });
@@ -61,12 +86,12 @@ const validateSignUp = async (email, otp) => {
     return [false, "user not found"];
   }
   if (user && user.otp !== otp) {
-    return [false, "Invalid OTP"];
+    return [false, "One time password doesn't match"];
   }
   const updatedUser = await userModel.findByIdAndUpdate(user._id, {
     activated: true,
   });
-  return [true, updatedUser];
+  return updatedUser;
 };
 
 const verifyEmail = async (req, res) => {
@@ -75,19 +100,4 @@ const verifyEmail = async (req, res) => {
   res.json({ data: user });
 };
 
-const logout = async (req, res) => {
-  const { otp } = req.body;
-  const user = await userModel.findOne({ otp });
-
-  if (!user) {
-    res.status(400).json({ message: "invalid otp" });
-  } else {
-    const updatedUser = await userModel.findOneAndUpdate(
-      { otp },
-      { activated: false, otp: null }
-    );
-    res.status(200).json({ message: "signed out" });
-  }
-};
-
-module.exports = { verifyEmail, signUp, logout };
+module.exports = { verifyEmail, signUp, logout, login };
