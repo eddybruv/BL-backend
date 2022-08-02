a93160771e0c266b660feae9cc8c058203769d22
eddybruv <edwinajong@gmail.com>
2022-08-02 21:18:53 +0100

    added login route and user image middleware

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,12 +1,20 @@
 const router = require("express").Router();
+const protect = require("../middleware/authMiddleware");
+const { uploader } = require("../controllers/image.controller");
+const multer = require("multer");
+const { storage } = require("../storage/storage");
+const upload = multer({ storage: storage });
 
 const {
   signUp,
   verifyEmail,
   logout,
+  login,
 } = require("../controllers/auth.controller");
 
-router.post("/register", signUp);
+router.post("/register", upload.single("image"), signUp);
+router.post("/login", login);
 router.post("/verify", verifyEmail);
+router.post("/logout", protect, logout);
 
-module.exports = router
+module.exports = router;
