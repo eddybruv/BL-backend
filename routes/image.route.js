25e21f113644f4d00e2e78b4a9b532e2aef5ded5
eddybruv <edwinajong@gmail.com>
2022-08-02 21:17:27 +0100

    worked on the file upload route (to be continued)

--- routes/image.route.js
+++ routes/image.route.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const { uploader } = require("../controllers/image.controller");
+const multer = require("multer");
+const { storage } = require("../storage/storage");
+const upload = multer({ storage: storage });
 
-router.post("/upload", uploader);
+router.post("/upload", upload.single("image"), uploader);
 
 module.exports = router;
