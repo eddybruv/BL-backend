a75e3c95ba9b08062f9e06f89e89321efcf91aa6
eddybruv <edwinajong@gmail.com>
2022-08-03 20:17:09 +0100

    created route

--- routes/category.route.js
+++ routes/category.route.js
@@ -1,5 +1,11 @@
 const router = require("express").Router();
-const {createCategory, getCategory, updateCategory, deleteCategory} = require("../controllers/category.controller");
+const {
+  createCategory,
+  getCategory,
+  updateCategory,
+  deleteCategory,
+  getCategoryById,
+} = require("../controllers/category.controller");
 const protect = require("../middleware/authMiddleware");
 const multer = require("multer");
 const { storage } = require("../storage/storage");
@@ -7,6 +13,7 @@ const upload = multer({ storage: storage });
 
 router.post("/create", upload.single("image"), protect, createCategory);
 router.get("/get", protect, getCategory);
+router.get("/get/:id", protect, getCategoryById);
 router.put("/update/:id", upload.single("image"), protect, updateCategory);
 router.delete("/delete/:id", protect, deleteCategory);
 
