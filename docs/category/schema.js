93d7919ef39173d270aac4825b61ff290b97ac08
eddybruv <edwinajong@gmail.com>
2022-08-03 16:43:21 +0100

    category schema

---
+++ docs/category/schema.js
@@ -0,0 +1,19 @@
+/**
+ * @swagger
+ * definitions:
+ *  Category:
+ *    type: object
+ *    properties:
+ *      _id:
+ *        type: integer
+ *      name:
+ *        type: string
+ *      description:
+ *        type: string
+ *      image:
+ *        type: string
+ *      createdAt:
+ *          type: string
+ *      updatedAt:
+ *          type: string
+ */
