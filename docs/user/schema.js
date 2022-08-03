838af2e8f754c29193e3e61dd195719b43d5a188
eddybruv <edwinajong@gmail.com>
2022-08-03 16:43:45 +0100

    user schema

---
+++ docs/user/schema.js
@@ -0,0 +1,27 @@
+/**
+ * @swagger
+ * definitions:
+ *  User:
+ *    type: object
+ *    properties:
+ *      _id:
+ *        type: integer
+ *      name:
+ *        type: string
+ *      username:
+ *        type: string
+ *      email:
+ *        type: string
+ *      password:
+ *        type: string
+ *      profilePicture:
+ *        type: string
+ *      otp: 
+ *        type: string
+ *      isActive:
+ *         type: string
+ *      createdAt:
+ *          type: string
+ *      updatedAt:
+ *          type: string
+ */
