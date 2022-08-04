86b4338d5924f8a6482834aa0101e94913027c1a
eddybruv <edwinajong@gmail.com>
2022-08-04 17:53:41 +0100

    changed to array to be able to upload multiple images

--- routes/image.route.js
+++ routes/image.route.js
@@ -4,6 +4,6 @@ const multer = require("multer");
 const { storage } = require("../storage/storage");
 const upload = multer({ storage: storage });
 
-router.post("/upload", upload.single("image"), uploader);
+router.post("/upload", upload.array("image"), uploader);
 
 module.exports = router;
